f185f84f412b54baf91f303386bcf13d9641ce47
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2021-07-19 13:13:40 -0400

    feat: Improve lib
    - Add flag to remove diacritics
    - Refactor code
    - Add optional prefered style for decorators

--- src/js/lib.ts
+++ src/js/lib.ts
@@ -3,23 +3,11 @@
 import defaultStyles from './default-styles.json';
 import defaultDecorators from './default-decorators.json';
 
-type CharacterMap = Map<string, string>;
-type StyledText = string;
-export type StyleFunction = (text: string, isCaseInsensitive?: boolean) => StyledText;
-export type CharacterMappingFunction = (map: CharacterMap) => StyleFunction;
-
-/**
- * Produces a string transformation function based on the given map.
- */
-export function createMappingFunction(map: Map<string, string>): StyleFunction {
-	return (text: string) => [...text].map((char) => map.get(char) ?? map.get(char.toLowerCase()) ?? char).join('');
-}
-
 /**
  * Decorates a character if it's not a combining mark.
  */
 export function decorateNonCombiningMark(char: string, decorator: string) {
-	if ((/\p{Mark}/giu).test(char)) {
+	if ((/\p{Mark}|\s/giu).test(char)) {
 		return char;
 	}
 
@@ -30,46 +18,60 @@ export function decorateNonCombiningMark(char: string, decorator: string) {
  * Encloses a character if it's not a combining mark.
  */
 export function encloseNonCombiningMark(char: string, left: string, right: string) {
-	if ((/\p{Mark}/giu).test(char)) {
+	if ((/\p{Mark}|\s/giu).test(char)) {
 		return char;
 	}
 
 	return `${left}${char}${right}`;
 }
 
+/**
+ * Removes the character if it is a combining mark.
+ */
+export function stripCombiningMark(char: string) {
+	if ((/\p{Mark}/giu).test(char)) {
+		return '';
+	}
+
+	return char;
+}
+
+type CharacterMap = Map<string, string>;
+type StyledText = string;
+export type StyleFunction = (text: string, stripAccents?: boolean) => StyledText;
+export type CharacterMappingFunction = (map: CharacterMap) => StyleFunction;
+
+/**
+ * Produces a string transformation function based on the given map.
+ */
+export function createMappingFunction(map: Map<string, string>): StyleFunction {
+	return (text: string, stripAccents = true) => {
+		let filteredText = [...text];
+
+		if (stripAccents) {
+			filteredText = filteredText.map(stripCombiningMark).filter((char) => char !== '');
+		}
+
+		return [...filteredText].map((char) => map.get(char) ?? map.get(char.toLowerCase()) ?? char).join('');
+	};
+}
+
 type TransformationFunctionParameter = (char: string, index: number, array: ThisParameterType<string[]>) => string;
-export type TransformationFunction = (text: string) => string;
 
 /**
  * Produces a string transformation function based on the given function.
  */
-export function createTransformationFunction(func: TransformationFunctionParameter): TransformationFunction {
-	return (text: string) => [...text].map(func).join('');
+export function createTransformationFunction(func: TransformationFunctionParameter): StyleFunction {
+	return (text: string, stripAccents = true) => {
+		let filteredText = [...text];
+
+		if (stripAccents) {
+			filteredText = filteredText.map(stripCombiningMark).filter((char) => char !== '');
+		}
+
+		return [...filteredText].map(func).join('');
+	};
 }
-
-export interface Decorator {
-	left: string,
-	right: string
-}
-export type DecoratorList = Record<string, Decorator>;
-type DecoratorsMap = Map<string, Decorator>;
-
-const DEFAULT_DECORATORS: DecoratorList = {
-	...Object.entries(defaultDecorators as Record<string, Decorator>).reduce((decorators, [name, decorator]) => {
-		decorators[name] = decorator;
-
-		return decorators;
-	}, {}),
-
-	'Next Year': {
-		left: (new Date().getFullYear() + 1).toString(),
-		right: (new Date().getFullYear() + 1).toString()
-	},
-	'Current Year': {
-		left: new Date().getFullYear().toString(),
-		right: new Date().getFullYear().toString()
-	}
-};
 
 type TextStyleList = Record<string, StyleFunction>;
 type StylesMap = Map<string, StyleFunction>;
@@ -116,20 +118,45 @@ const DEFAULT_STYLES: TextStyleList = {
 		const repeatTimes = Math.floor(Math.random() * RANDOM_CONST);
 
 		return decorateNonCombiningMark(char, char.repeat(repeatTimes));
-	}),
-	'New Year': (text) => {
-		/* eslint-disable new-cap */
-		const left = DEFAULT_STYLES['Lower Numbers'](DEFAULT_DECORATORS['Current Year'].left);
-		const right = DEFAULT_STYLES['Upper Numbers'](DEFAULT_DECORATORS['Next Year'].right);
-		/* eslint-enable new-cap */
+	})
+};
 
-		return `~${left}~ ${text} ~${right}~`;
+export interface Decorator {
+	left: string,
+	right: string,
+	preferedStyle?: string
+}
+export type DecoratorList = Record<string, Decorator>;
+type DecoratorsMap = Map<string, Decorator>;
+
+const DEFAULT_DECORATORS: DecoratorList = {
+	...Object.entries(defaultDecorators as Record<string, Decorator>).reduce((decorators, [name, decorator]) => {
+		decorators[name] = decorator;
+
+		return decorators;
+	}, {}),
+
+	'Next Year': {
+		left: (new Date().getFullYear() + 1).toString(),
+		right: (new Date().getFullYear() + 1).toString()
+	},
+	'Current Year': {
+		left: new Date().getFullYear().toString(),
+		right: new Date().getFullYear().toString()
+	},
+	'New Year': {
+		// eslint-disable-next-line new-cap
+		left: DEFAULT_STYLES['Lower Numbers'](new Date().getFullYear().toString()),
+		// eslint-disable-next-line new-cap
+		right: DEFAULT_STYLES['Upper Numbers']((new Date().getFullYear() + 1).toString())
 	}
 };
 
 export interface TextDecoratorOptions {
 	leftDecorator?: string,
-	rightDecorator?: string
+	rightDecorator?: string,
+	normalizeResult?: boolean,
+	stripAccents?: boolean
 }
 
 export class TextDecorator {
@@ -149,10 +176,12 @@ export class TextDecorator {
 	/**
 	 * Decorate a string with the given style.
 	 */
-	decorateText(text: string, style = 'leet', options: TextDecoratorOptions = {}) {
+	decorateText(text: string, style = 'nostyle', options: TextDecoratorOptions = {}) {
 		const resolvedOptions = {
 			leftDecorator: '',
 			rightDecorator: '',
+			normalizeResult: false,
+			stripAccents: true,
 			...options
 		};
 
@@ -160,8 +189,12 @@ export class TextDecorator {
 		const rightDecorator = this.#decorators.get(resolvedOptions.rightDecorator)?.right ?? '';
 
 		const normalizedText = text.normalize('NFD');
-		const transformedText = this.#styles.get(style)?.(normalizedText) ?? normalizedText;
-		const finalText = transformedText.normalize('NFC');
+		const transformedText = this.#styles.get(style)?.(normalizedText, resolvedOptions.stripAccents) ?? normalizedText;
+		let finalText = transformedText;
+
+		if (resolvedOptions.normalizeResult) {
+			finalText = transformedText.normalize('NFC');
+		}
 
 		return `${leftDecorator}${finalText}${rightDecorator}`;
 	}
@@ -170,8 +203,10 @@ export class TextDecorator {
 	addStyle = (name: string, style: StyleFunction) => this.#styles.set(name, style);
 	removeStyle = (name: string) => this.#styles.delete(name);
 	listStyles = () => [...this.#styles.keys()];
+	getStyle = (name: string) => this.#styles.get(name);
 
 	addDecorator = (name: string, decorator: Decorator) => this.#decorators.set(name, decorator);
 	removeDecorator = (name: string) => this.#decorators.delete(name);
 	listDecorators = () => [...this.#decorators.keys()];
+	getDecorator = (name: string) => this.#decorators.get(name);
 }
