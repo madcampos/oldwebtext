032c5c1c40a1741006ea9d0b89148d63e3a804ab
madcampos <madcampos@outlook.com>
2024-02-17 01:12:16 -0500

    feat: small tweaks to app

--- src/js/lib.ts
+++ src/js/lib.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
 import defaultStyles from './default-styles';
 import defaultDecorators from './default-decorators';
 
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-console*/
-
 import { TextDecorator } from './lib';
 
 document.addEventListener('DOMContentLoaded', () => {
